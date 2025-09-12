3ee8ba6b6ff3d7702d27537dff68f27a10cc6455
Jared Mollenkamp <jared@mollenkamp.com>
2025-09-12 13:25:36 -0500

    Refactor details component to use HousingLocationInfo and update template bindings

--- src/app/details/details.ts
+++ src/app/details/details.ts
@@ -1,11 +1,7 @@
 import {Component, inject} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {HousingService} from '../housing.service';
-<<<<<<< HEAD
 import {SiteInfo} from '../siteinfo';
-=======
-import {HousingLocationInfo} from '../housinglocation';
->>>>>>> origin/main
 
 @Component({
   selector: 'app-details',
@@ -14,7 +10,6 @@ import {HousingLocationInfo} from '../housinglocation';
     <article>
       <img
         class="listing-photo"
-<<<<<<< HEAD
         [src]="siteInfo?.photo"
         alt="Exterior photo of {{ siteInfo?.name }}"
         crossorigin
@@ -29,50 +24,21 @@ import {HousingLocationInfo} from '../housinglocation';
           <li>Posted: {{ siteInfo?.checked }}</li>
           <li>Should fail: {{ siteInfo?.should_fail }}</li>
           <li>Should Succeed: {{ siteInfo?.should_succeed }}</li>
-=======
-        [src]="housingLocation?.photo"
-        alt="Exterior photo of {{ housingLocation?.name }}"
-        crossorigin
-      />
-      <section class="listing-description">
-        <h2 class="listing-heading">{{ housingLocation?.name }}</h2>
-        <p class="listing-location">{{ housingLocation?.city }}, {{ housingLocation?.state }}</p>
-      </section>
-      <section class="listing-features">
-        <h2 class="section-heading">About this housing location</h2>
-        <ul>
-          <li>Units available: {{ housingLocation?.availableUnits }}</li>
-          <li>Does this location have wifi: {{ housingLocation?.wifi }}</li>
-          <li>Does this location have laundry: {{ housingLocation?.laundry }}</li>
->>>>>>> origin/main
         </ul>
       </section>
     </article>
   `,
-<<<<<<< HEAD
-=======
-  styleUrls: ['./details.css']
->>>>>>> origin/main
 })
 
 export class Details {
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService = inject(HousingService);
-<<<<<<< HEAD
   siteInfo: SiteInfo | undefined;
 
   constructor() {
     const siteInfoId = Number(this.route.snapshot.params['id']);
     this.housingService.getSiteInfoById(siteInfoId).then(location => {
       this.siteInfo = location;
-=======
-  housingLocation: HousingLocationInfo | undefined;
-
-  constructor() {
-    const housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingService.getHousingLocationById(housingLocationId).then(location => {
-      this.housingLocation = location;
->>>>>>> origin/main
     });
   }
 }
