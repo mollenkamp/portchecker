a0de4e4658a065e662ad7d3c533d8722a513eb9c
Jared Mollenkamp <jared@mollenkamp.com>
2025-09-22 17:44:34 -0500

    Updates

--- updateuat.sh
+++ updateuat.sh
@@ -2,12 +2,15 @@ rm ./public/*.png
 rm ./updateuat.zip
 cd ~/Desktop/GitHub/portchecker
 rm -Rf ./dist
-ng build
-cd dist/portchecker/browser
+rm -Rf ./.angular
+rm -Rf ./node_modules
+npm install
+ng build --configuration production --output-hashing all --aot
+cd ./dist/portchecker/browser
 zip -r ./updateuat.zip ./*
 scp -P 18765 -i ~/.ssh/id_ed25519 ./updateuat.zip u2528-gpezwd7o7ya7@ssh.freshupgrades.com:~/www/port-uat.freshupgrades.com/updateuat.zip
 ssh u2528-gpezwd7o7ya7@ssh.freshupgrades.com -p 18765 -i ~/.ssh/id_ed25519 'rm -Rf ~/www/port-uat.freshupgrades.com/public_html'
 ssh u2528-gpezwd7o7ya7@ssh.freshupgrades.com -p 18765 -i ~/.ssh/id_ed25519 'unzip -o ~/www/port-uat.freshupgrades.com/updateuat.zip -d ~/www/port-uat.freshupgrades.com/public_html'
 ssh u2528-gpezwd7o7ya7@ssh.freshupgrades.com -p 18765 -i ~/.ssh/id_ed25519 'rm ~/www/port-uat.freshupgrades.com/updateuat.zip'
-
+ssh u2528-gpezwd7o7ya7@ssh.freshupgrades.com -p 18765 -i ~/.ssh/id_ed25519 'rm -Rf ~/.opcache'
 rm ./updateuat.zip
